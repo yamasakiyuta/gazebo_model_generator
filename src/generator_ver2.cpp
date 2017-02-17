45a5213d8e419abcc4ded7610c68c81839c06b62
Yuta Yamasaki <ee33114@meiji.ac.jp>
2017-02-17 17:58:19 +0900

    モデルコンフィグファイル生成機能を追加

--- src/generator_ver2.cpp
+++ src/generator_ver2.cpp
@@ -12,31 +12,52 @@ void sdf_generator(std::string output,std::string collada,std::string model_name
 
     // ファイルに1行ずつ書き込み
     ofs << "<?xml version='1.0'?>" << std::endl;
-    ofs <<"	<sdf version='1.4'>" << std::endl;
-    ofs <<"	<model name='"+ model_name +"'>" << std::endl;
-    ofs <<"	<static>true</static>" << std::endl;
-    ofs <<"		<link name='link'>" << std::endl;
-    ofs <<"			<pose>0 0 0 0 0 0</pose>" << std::endl;
-    ofs <<"				<collision name='collision'>" << std::endl;
-    ofs <<"					<geometry>" << std::endl;
-    ofs <<"						<mesh><uri>model://"+model_name+"/meshes/"+collada+"</uri></mesh>" << std::endl;
-    ofs <<"					</geometry>" << std::endl;
-    ofs <<"				</collision>" << std::endl;
-    ofs <<"				<visual name='visual1'>" << std::endl;
-    ofs <<"					<geometry>" << std::endl;
-    ofs <<"						<mesh><uri>model://"+model_name+"/meshes/"+collada+"</uri></mesh>" << std::endl;
-    ofs <<"					</geometry>" << std::endl;
-    ofs <<"    	         <material>" << std::endl;
-    ofs <<"                <script>" << std::endl;
-    ofs <<"                 <uri>model://asphalt_plane/materials/scripts</uri>" << std::endl;
-    ofs <<"                 <uri>model://asphalt_plane/materials/textures</uri>" << std::endl;
-    ofs <<"                 <name>vrc/asphalt</name>" << std::endl;
-    ofs <<"               </script>" << std::endl;
-    ofs <<"              </material>" << std::endl;
-    ofs <<"				</visual>" << std::endl;
-    ofs <<"		</link>" << std::endl;
-    ofs <<"	</model>" << std::endl;
+    ofs << "    <sdf version='1.4'>" << std::endl;
+    ofs << "    <model name='"+ model_name +"'>" << std::endl;
+    ofs << "    <static>true</static>" << std::endl;
+    ofs << "		<link name='link'>" << std::endl;
+    ofs << "			<pose>0 0 0 0 0 0</pose>" << std::endl;
+    ofs << "				<collision name='collision'>" << std::endl;
+    ofs << "					<geometry>" << std::endl;
+    ofs << "						<mesh><uri>model://"+model_name+"/meshes/"+collada+"</uri></mesh>" << std::endl;
+    ofs << "					</geometry>" << std::endl;
+    ofs << "				</collision>" << std::endl;
+    ofs << "				<visual name='visual1'>" << std::endl;
+    ofs << "					<geometry>" << std::endl;
+    ofs << "						<mesh><uri>model://"+model_name+"/meshes/"+collada+"</uri></mesh>" << std::endl;
+    ofs << "					</geometry>" << std::endl;
+    ofs << "					<material>" << std::endl;
+    ofs << "                 	<script>" << std::endl;
+    ofs << "                 		<uri>model://asphalt_plane/materials/scripts</uri>" << std::endl;
+    ofs << "                 		<uri>model://asphalt_plane/materials/textures</uri>" << std::endl;
+    ofs << "                 		<name>vrc/asphalt</name>" << std::endl;
+    ofs << "                	</script>" << std::endl;
+    ofs << "                	</material>" << std::endl;
+    ofs << "				</visual>" << std::endl;
+    ofs << "		</link>" << std::endl;
+    ofs << "	</model>" << std::endl;
     ofs << "</sdf>" << std::endl;
+}
+
+void cfg_generator(std::string output,std::string collada,std::string model_name)
+{
+    // ファイル出力ストリームの初期化
+    std::ofstream ofs(output.c_str());
+
+    // ファイルに1行ずつ書き込み
+    ofs << "<?xml version='1.0'?>" << std::endl;
+    ofs << "<model>" << std::endl;
+    ofs << "  <name>" + model_name + "</name>" << std::endl;
+    ofs << "  <version>1.0</version>" << std::endl;
+    ofs << "  <sdf version='1.4'>model.sdf</sdf>" << std::endl;
+    ofs << "  <author>" << std::endl;
+    ofs << "    <name>My Name</name>" << std::endl;
+    ofs << "    <email>me@my.email</email>" << std::endl;
+    ofs << "  </author>" << std::endl;
+    ofs << "  <description>" << std::endl;
+    ofs << "    My awsome model." << std::endl;
+    ofs << "  </description>" << std::endl;
+    ofs << "<model>" << std::endl;
 }
 
 void make_model_dir(std::string model_name)
@@ -90,11 +111,13 @@ int main(int argc, char* argv[])
 	std::string fullpath = argv[1];
     std::string filename = get_filename(argv[1]); //gazeboのモデルに使用するメッシュ(.dae)
     std::string model_name = argv[2]; //gazeboモデルの名前
-    std::string output = "models/" + model_name + "/model.sdf";
-    printf("output file : %s\n",output.c_str());
+    std::string output_sdf = "models/" + model_name + "/model.sdf";
+    std::string output_cfg = "models/" + model_name + "/model.cfg";
+    //printf("output file : %s\n",output.c_str());
     
     make_model_dir(model_name);//フォルダ作成
-    sdf_generator(output, filename, model_name);//ファイル作成
+    sdf_generator(output_sdf, filename, model_name);//ファイル作成
+    cfg_generator(output_cfg, filename, model_name);//ファイル作成
 	move_meshfile(fullpath, filename, model_name);//ファイル移動
 
     return 0;
